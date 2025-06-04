465d679eb642bf2514e3ff41005d1882ac9687b9
SanyaSriv <ssrivas7@ucsc.edu>
2025-06-04 06:10:26 -0400

    Added invariants and the merging logic

--- attempt_1/main.py
+++ attempt_1/main.py
@@ -29,9 +29,10 @@ class DisjointSetForest:
         root_y = self.find_root(y)
 
         if root_x == root_y:
-            return
+            return 0 # sets were already merged
 
         self.parent[root_y] = root_x # will be compressed when find_root is called on it later on
+        return 1 # some change was made
 
     def pretty_print(self):
         root_sets = {}
@@ -55,8 +56,8 @@ class Protocol:
         self.states = []
         self.messages = []
         self.causal_relationships = {}
-        self.stalling = {}
-        self.not_stalling = {}
+        self.not_processable = {}
+        self.processable = {}
     
     def extract_messages(self):
         """
@@ -94,7 +95,7 @@ class Protocol:
                 msg1 = self.data[i].split(',')[1].strip()
                 for j in range(i, 0, -1):
                     if "case" in self.data[j]:
-                        msg2 = self.data[j].strip().split(" ")[1].replace(",", "").replace(" ", "")
+                        msg2 = self.data[j].strip().split(" ")[1].replace(",", "").replace(" ", "").replace(":", "")
                         if msg2 in self.causal_relationships:
                             if msg1 not in self.causal_relationships[msg2]:
                                 self.causal_relationships[msg2].append(msg1)
@@ -109,15 +110,29 @@ class Protocol:
         print("causal relationships", self.causal_relationships)
 
 
-    def generate_stalling_not_stalling(self, stalling, not_stalling):
+    def generate_not_processable_processable(self, not_processable, processable):
         # TODO
-        if len(stalling) == 0 or len(not_stalling) == 0:
-            # generate the stalling dictionary by running the Murphi code
+        if len(not_processable) == 0:
             pass
+        else:
+            self.not_processable = not_processable
 
-        self.stalling = stalling
-        self.not_stalling = not_stalling
-
+        if len(processable) == 0:
+            find_state_case = r'\s*case\s*(.*?):\s*'
+            for i in range(0, len(self.data)):
+                state_match = re.match(find_state_case, self.data[i])
+                if state_match:
+                    state_name = state_match.group(1)
+                    self.processable[state_name] = []
+                    if state_name in self.states:
+                        # proceed to find which messages can this state process
+                        for j in range(i+1, len(self.data)):
+                            if "endswitch" in self.data[j]:
+                                break
+                            if "case" in self.data[j]:
+                                self.processable[state_name].append(self.data[j].strip().split(" ")[1].replace(":", "").replace(" ", ""))
+        else:
+            self.processable = processable      
         
 
 def make_sets(messages, states, causal_relationships):
@@ -133,6 +148,36 @@ def make_sets(messages, states, causal_relationships):
     M_Sets.add_causality(causal_relationships) # add the causal relationships between messages
 
     return M_Sets
+
+def merge_sets(M_sets, protocol):
+    """
+    Function responsible for merging the sets such that all the invariants are respected
+    """
+    # keep merging till changes are being made
+    change_made = 1 # starting with 1 so we can do the initial loop
+    while change_made:
+        change_made = 0
+        for i in protocol.messages:
+            for j in protocol.messages:
+                if i != j:
+                    # invariant 1: we can merge two states that there exists two states s and s', such that one message is processed by s and the other is processed by s'
+                    s1 = any(i in protocol.processable[p] for p in protocol.processable)
+                    s2 = any(j in protocol.processable[p] for p in protocol.processable)
+                    
+                    # invariant 2 (part 1):
+                    # if i is processable, then it is not possible for j to be in some state's not_processable list
+                    s3 = any(i in protocol.processable[p] for p in protocol.processable)
+                    s4 = any(j in protocol.not_processable[p] for p in protocol.not_processable)
+                    
+                    # invariant 2 (part 2): 
+                    # if j is processable, then it is not possible for i to be in some state's processable list
+                    s5 = any(i in protocol.not_processable[p] for p in protocol.not_processable)
+                    s6 = any(j in protocol.processable[p] for p in protocol.processable)
+
+                    # TODO: add the causality invariant to avoid deadlocks between VNs - avoiding that for now
+
+                    if (s1 and s2) and not (s3 and s4) and not (s5 and s6):
+                        change_made |= M_sets.union(i, j) # typically here, we will take causality into account to see if we should merge x into y or y into x
 
 def get_link_information(data):
     pass
@@ -154,17 +199,24 @@ def main():
     # extract the states, messages, and causal relationships from the Murphi model
     protocol.extract_messages()
     protocol.extract_states()
-    protocol.generate_stalling_not_stalling(meta_data['stalling'], meta_data['not_stalling'])
+    protocol.generate_not_processable_processable(meta_data['not_processable'], meta_data['processable'])
     protocol.extract_causal_relationships()
 
-    print("Stalling dictionary = ", protocol.stalling)
-    print("Not stalling dictionary = ", protocol.not_stalling)
+    print("Not processable dictionary = ", protocol.not_processable)
+    print("Processable dictionary = ", protocol.processable)
     print("List of all messages = ", protocol.messages)
     print("List of all states = ", protocol.states)
+    print("Causal relationships = ", protocol.causal_relationships)
 
     # Now we start the algorithm
+    print("-----------------------START ALGORITHM------------------------")
+    print("*** Printing initial sets ***")
     M_sets = make_sets(protocol.messages, protocol.states, protocol.causal_relationships)
     M_sets.pretty_print() # printing the initial list
+
+    print("*** Printing merged sets ***")
+    merge_sets(M_sets, protocol)
+    M_sets.pretty_print() 
 
 if __name__ == "__main__":
     main()
